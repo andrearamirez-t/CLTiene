cc9eb81213ae951ee17495be6a2b241ce4c92811
Esteban <esteban_sernap@cun.edu.co>
2026-03-11 17:51:57 -0500

    Pequeña correción

--- back/api/database.py
+++ back/api/database.py
@@ -24,12 +24,17 @@ def result(query: str, query_parameters: list = []):
 def option(query: str, column_id: str, column_name: str | None = None):
     column_name = column_name if column_name else column_id
 
-    job = client.query(f"""
+    o_query = f"""
     -- Solo valido que el ID no este vacio y agrupo los nombres para que no se repitan
     with result as (
         {query}
-    ) select {column_id} id, {column_name} name from result where {column_id} is not null group by {column_name}
-    """)
+    ) select {column_id} id, {column_name} name from result where {column_id} is not null
+    -- group by {column_name}
+    """
+
+    print(o_query)
+
+    job = client.query(o_query)
     df = job.to_dataframe()
 
     if df.empty:
