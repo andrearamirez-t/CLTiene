351586e9fdbb5c1a8a0ef38659e4e1effa35d040
Esteban <esteban_sernap@cun.edu.co>
2026-03-13 10:22:34 -0500

    Funcion para correcion de fechas

--- back/api/database.py
+++ back/api/database.py
@@ -41,3 +41,34 @@ def option(query: str, column_id: str, column_name: str | None = None):
         return {}
 
     return df.to_dict(orient="records")
+
+
+def calculo_fecha() -> str:
+    return """
+    DATETIME (
+		PARSE_TIMESTAMP (
+			'%Y-%m-%d %H:%M:%S',
+			CASE
+			    -- formato: 2026-01-28 09:40:17 (creo que es ISO o el formato estandar año/mes/dia hora-minuto-segundo)
+				WHEN REGEXP_CONTAINS (fecha, r'^\\d{{4}}-\\d{{2}}-\\d{{2}}') THEN REGEXP_EXTRACT (fecha, r'(\\d{{4}}-\\d{{2}}-\\d{{2}} \\d{{2}}:\\d{{2}}:\\d{{2}})')
+
+				-- formato: 1/11/2025 2:42:44 p. m. (Formato en español dia-mes-año hora-minuto-segundo)
+				ELSE FORMAT_TIMESTAMP (
+					'%Y-%m-%d %H:%M:%S',
+					PARSE_TIMESTAMP (
+						'%d/%m/%Y %H:%M:%S',
+						CONCAT (
+							REGEXP_EXTRACT (fecha, r'(\\d{{1,2}}/\\d{{1,2}}/\\d{{4}})'),
+							' ',
+							REGEXP_EXTRACT (fecha, r'(\\d{{1,2}}:\\d{{2}}:\\d{{2}})')
+						)
+					) + IF (
+						REGEXP_CONTAINS (fecha, r'(?i)p'),
+						INTERVAL 12 HOUR,
+						INTERVAL 0 HOUR
+					)
+				)
+			END
+		)
+	)
+    """
